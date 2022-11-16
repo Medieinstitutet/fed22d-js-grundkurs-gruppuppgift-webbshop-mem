6a27b03c441d99f36ab6ebc8645af9677bf034ba
Mattias heurlin <mattias.heurlin@hotmail.com>
2022-11-16 23:58:14 +0100

    JS compressed

--- javascript.js
+++ javascript.js
@@ -3,15 +3,15 @@ const menuLinks = document.getElementsByClassName('menu_links')[0];
 
 let menuOpen = false; //Menyn är stängd som default
 
-menuButton.addEventListener('click', () =>{
-    menuOpen =!menuOpen; //Gör att menyn får värdet true
-    if(!menuOpen){
+menuButton.addEventListener('click', () => {
+    menuOpen = !menuOpen; //Gör att menyn får värdet true
+    if (!menuOpen) {
         menuButton.blur();// Tar bort fokus från knappen
-    } 
-    if(menuOpen){
+    }
+    if (menuOpen) {
         menuButton.classList.add('active');
         menuLinks.classList.add('active');
-    }else{
+    } else {
         menuButton.classList.remove('active');
         menuLinks.classList.remove('active');
     }
@@ -20,17 +20,17 @@ menuButton.addEventListener('click', () =>{
 
 //Tar bort active när man klickar på en av länkarna. Gäller alla länkar
 document.querySelectorAll('.list_item').forEach(link => link.
-    addEventListener('click', () =>{ //Be om hjälp för att skriva om detta
+    addEventListener('click', () => { //Be om hjälp för att skriva om detta
         menuOpen = false;
         menuButton.blur();
-if(menuOpen){
+        if (menuOpen) {
             menuButton.classList.add('active');
             menuLinks.classList.add('active');
-        }else{
+        } else {
             menuButton.classList.remove('active');
             menuLinks.classList.remove('active');
         }
-}))
+    }))
 
 
 
@@ -133,6 +133,12 @@ const donuts =
         )
     ];
 const donutPlacement = document.querySelectorAll(".donuts");
+let plusbtn = document.querySelectorAll('button[data-operator="plus"]');
+let minusbtn = document.querySelectorAll('button[data-operator="minus"]');
+for (let i = 0; i < plusbtn.length; i++) {
+    plusbtn[i].addEventListener('click', countUp);
+    minusbtn[i].addEventListener('click', countDown);
+}
 /* fungerar ej att lägga in denna i donutMarkup ännu 
 <figure>
        <img src="${donuts[i].picSrc}" alt="${donuts[i].name}>
@@ -140,170 +146,134 @@ const donutPlacement = document.querySelectorAll(".donuts");
    </figure>
 */ // Ska kolla med Jenni hur man löser det
 
-for (let i = 0; i < donuts.length; i++) { // Denna skriver ut alla våra donuts
-    let donutNr = "nr" + i;
-    const donutMarkup = `
-    <div class="${donutNr} donuts">
-    <img src="${donuts[i].picSrc}" width="130px" height="130">
-   <h4>${donuts[i].name}</h4>
-   <ul>
-       <li>${donuts[i].price}kr</li>
-       <li>Innerhåller: ${donuts[i].aspect}</li>
-   </ul>
-   </div>
+// Funktion lista nedan
+
+function displayDonut1() {
+    for (let i = 0; i < donuts.length; i++) { // Denna skriver ut alla våra donuts
+        let donutNr = "nr" + i;
+        const donutMarkup = `
+            <div class="${donutNr} donuts">
+             <img src="${donuts[i].picSrc}" width="130px" height="130">
+            <h4>${donuts[i].name}</h4>
+            <ul>
+              <li>${donuts[i].price}kr</li>
+              <li>Innerhåller: ${donuts[i].aspect}</li>
+            </ul>
+        </div>
    `;
-    donutPlacement[i].outerHTML = donutMarkup;
+        donutPlacement[i].outerHTML = donutMarkup;
+    }
 }
-const plusbtn = document.querySelectorAll('button[data-operator="plus"]');
-const minusbtn = document.querySelectorAll('button[data-operator="minus"]');
-for (let i = 0; i < plusbtn.length; i++) {
-    plusbtn[i].addEventListener('click', countUp);
-    minusbtn[i].addEventListener('click', countDown);
+
+function displayDonutCart() {
+    const selectedOrderplacment = document.querySelectorAll(".selectedOrder")
+    for (let i = 0; i < donuts.length; i++) {
+        let donutNr = "nr" + i;
+        const cartOrderMarkup = `
+            <div class="${donutNr} donuts">
+            <img src="${donuts[i].picSrc}" width="150px" height="150">
+            <h4>${donuts[i].name}</h4>
+            <ul>
+               <li>${donuts[i].price}kr</li>
+               <li>Antal: ${donuts[i].selectCounter}</li>
+             </ul>
+             <div class="plus_minusBtn">
+                        <button class="btn_cart_plus">+</button>
+                        <button class="btn_cart_minus">-</button>
+            </div>
+            </div>
+        `;
+        if (donuts[i].selectCounter > 0) {
+            selectedOrderplacment[i].innerHTML = cartOrderMarkup;
+        }
+    }
 }
+function calcTotalorder() {
+    const totalAmountPlacement = document.querySelector(".total_amount");
+    let = totalAmount = 0;
+    orderListAmount = 0;
+    for (let i = 0; i < donuts.length; i++) {
+        if (donuts[i].selectCounter > 0) {
+            let = combinedAmount = 0;
+            combinedAmount = donuts[i].price * donuts[i].selectCounter;
+            totalAmount = totalAmount + combinedAmount;
+            orderListAmount++;
+        }
+    }
+    totalAmountPlacement.innerHTML = totalAmount + "kr";
+}
+// ----------------------------
+
+displayDonut1();
+
+
 function countUp(e) {
     const controll = e.currentTarget.parentElement.parentElement.firstElementChild.attributes.class;
     const updateCounter = document.querySelectorAll(".selectcounter");
-    switch (controll.value) {
-
-        case 'nr0 donuts':
-            donuts[0].selectCounter++;
-            updateCounter[0].innerHTML = donuts[0].selectCounter;
-            break;
-        case 'nr1 donuts':
-            donuts[1].selectCounter++;
-            updateCounter[1].innerHTML = donuts[1].selectCounter;
-            break;
-        case 'nr2 donuts':
-            donuts[2].selectCounter++;
-            updateCounter[2].innerHTML = donuts[2].selectCounter;
-            break;
-        case 'nr3 donuts':
-            donuts[3].selectCounter++;
-            updateCounter[3].innerHTML = donuts[3].selectCounter;
-            break;
-        case 'nr4 donuts':
-            donuts[4].selectCounter++;
-            updateCounter[4].innerHTML = donuts[4].selectCounter;
-            break;
-        case 'nr5 donuts':
-            donuts[5].selectCounter++;
-            updateCounter[5].innerHTML = donuts[5].selectCounter;
-            break;
-        case 'nr6 donuts':
-            donuts[6].selectCounter++;
-            updateCounter[6].innerHTML = donuts[6].selectCounter;
-            break;
-        case 'nr7 donuts':
-            donuts[7].selectCounter++;
-            updateCounter[7].innerHTML = donuts[7].selectCounter;
-            break;
-        case 'nr8 donuts':
-            donuts[8].selectCounter++;
-            updateCounter[8].innerHTML = donuts[8].selectCounter;
-            break;
-        case 'nr9 donuts':
-            donuts[9].selectCounter++;
-            updateCounter[9].innerHTML = donuts[9].selectCounter;
-            break;
+    for (let i = 0; i < donuts.length; i++) {
+        if (controll.value == 'nr' + i + ' donuts') {
+            donuts[i].selectCounter++;
+            updateCounter[i].innerHTML = donuts[i].selectCounter;
+        }
     }
 }
 
 function countDown(e) {
     const controll = e.currentTarget.parentElement.parentElement.firstElementChild.attributes.class;
     const updateCounter = document.querySelectorAll(".selectcounter");
-    switch (controll.value) {
-
-        case 'nr0 donuts':
-            if (donuts[0].selectCounter <= 0)
+    for (let i = 0; i < donuts.length; i++) {
+        for (let i = 0; i < donuts.length; i++) {
+            if (donuts[i].selectCounter <= 0) {
                 return;
-            donuts[0].selectCounter--;
-            updateCounter[0].innerHTML = donuts[0].selectCounter;
-            break;
-        case 'nr1 donuts':
-            if (donuts[1].selectCounter <= 0)
-                return;
-            donuts[1].selectCounter--;
-            updateCounter[1].innerHTML = donuts[1].selectCounter;
-            break;
-        case 'nr2 donuts':
-            if (donuts[2].selectCounter <= 0)
-                return;
-            donuts[2].selectCounter--;
-            updateCounter[2].innerHTML = donuts[2].selectCounter;
-            break;
-        case 'nr3 donuts':
-            if (donuts[3].selectCounter <= 0)
-                return;
-            donuts[3].selectCounter--;
-            updateCounter[3].innerHTML = donuts[3].selectCounter;
-            break;
-        case 'nr4 donuts':
-            if (donuts[4].selectCounter <= 0)
-                return;
-            donuts[4].selectCounter--;
-            updateCounter[4].innerHTML = donuts[4].selectCounter;
-            break;
-        case 'nr5 donuts':
-            if (donuts[5].selectCounter <= 0)
-                return;
-            donuts[5].selectCounter--;
-            updateCounter[5].innerHTML = donuts[5].selectCounter;
-            break;
-        case 'nr6 donuts':
-            if (donuts[6].selectCounter <= 0)
-                return;
-            donuts[6].selectCounter--;
-            updateCounter[6].innerHTML = donuts[6].selectCounter;
-            break;
-        case 'nr7 donuts':
-            if (donuts[7].selectCounter <= 0)
-                return;
-            donuts[7].selectCounter--;
-            updateCounter[7].innerHTML = donuts[7].selectCounter;
-            break;
-        case 'nr8 donuts':
-            if (donuts[8].selectCounter <= 0)
-                return;
-            donuts[8].selectCounter--;
-            updateCounter[8].innerHTML = donuts[8].selectCounter;
-            break;
-        case 'nr9 donuts':
-            if (donuts[9].selectCounter <= 0)
-                return;
-            donuts[9].selectCounter--;
-            updateCounter[9].innerHTML = donuts[9].selectCounter;
-            break;
+            }
+            else if (controll.value == 'nr' + i + ' donuts') {
+                donuts[i].selectCounter--;
+                updateCounter[i].innerHTML = donuts[i].selectCounter;
+            }
+        }
     }
 }
+
 function toggleOrderPage() { // Togglar synligheten på varukorgen + placerar ut donuts med mer än 0 i antal + räknar ut totalen
     shoppingCartPage.classList.toggle("toggle-hidden");
-    const selectedOrderplacment = document.querySelectorAll(".selectedOrder")
+    displayDonutCart();
+    calcTotalorder();
+    let shopCartBtnUp = document.querySelectorAll(".btn_cart_plus")
+    let shopCartBtnDown = document.querySelectorAll(".btn_cart_minus")
     for (let i = 0; i < donuts.length; i++) {
-        const cartOrderMarkup = `
-            <img src="${donuts[i].picSrc}" width="150px" height="150">
-            <h4>${donuts[i].name}</h4>
-            <ul>
-               <li>${donuts[i].price}kr</li>
-               <li>Antal: ${donuts[i].selectCounter}</li>
-          </ul>
-        `;
-        if (donuts[i].selectCounter > 0) {
-            selectedOrderplacment[i].innerHTML = cartOrderMarkup;
+        console.log("test");
+        shopCartBtnUp[i].addEventListener('click', countUpCart);
+        shopCartBtnDown[i].addEventListener('click', countDown);
+    }
+    function countUpCart(e) { // funkar bara en gång
+        const controll= e.currentTarget.parentElement.parentElement.attributes.class;
+        const updateCounter = document.querySelectorAll(".selectcounter");
+        console.log(controll.value);
+        for (let i = 0; i < donuts.length; i++) {
+            if (controll.value == 'nr' + i + ' donuts') {
+                donuts[i].selectCounter++;
+                console.log(donuts[i].selectCounter);
+                displayDonutCart();
+            }
         }
+        
+        console.log('displayed');
     }
 
-    const totalAmountPlacement = document.querySelector(".total_amount");
-    let = totalAmount = 0; 
-    for (let i = 0; i < donuts.length; i++) {
-        if (donuts[i].selectCounter > 0) {
-            let = combinedAmount = 0;
-            combinedAmount = donuts[i].price * donuts[i].selectCounter; 
-            totalAmount = totalAmount + combinedAmount;
+    // Nedan ej klar
+    function countDown(e) {
+        const controll = e.currentTarget.parentElement.parentElement.firstElementChild.attributes.class;
+        const updateCounter = document.querySelectorAll(".selectcounter");
+        for (let i = 0; i < donuts.length; i++) {
+            for (let i = 0; i < donuts.length; i++) {
+                if (donuts[i].selectCounter <= 0) {
+                    return;
+                }
+                else if (controll.value == 'nr' + i + ' donuts') {
+                    donuts[i].selectCounter--;
+                    updateCounter[i].innerHTML = donuts[i].selectCounter;
+                }
+            }
         }
     }
-    totalAmountPlacement.innerHTML = totalAmount + "kr";
 }
-
-
-
-
