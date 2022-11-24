657aa5417c992894c3e1e41038a49b556a254ee4
Mattias heurlin <mattias.heurlin@hotmail.com>
2022-11-24 14:03:58 +0100

    Merge fixes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 const menuButton = document.getElementsByClassName('toggle_menu')[0];
 const menuLinks = document.getElementsByClassName('menu_links')[0];
 
@@ -35,34 +33,35 @@ document.querySelectorAll('.list_item').forEach((link) =>
     }
 
     menuButton.setAttribute('aria-expanded', true);
-}))
+  })
+);
 
 //Tar bort active när man klickar på en av länkarna. Gäller alla länkar
-document.querySelectorAll('.list_item').forEach((link) => 
-    link.addEventListener('click', () => { //Be om hjälp för att skriva om detta
-        menuOpen = false;
-        menuButton.blur();
-        if (menuOpen) {
-            menuButton.classList.add('active');
-            menuLinks.classList.add('active');
-        } else {
-            menuButton.classList.remove('active');
-            menuLinks.classList.remove('active');
-        }
-    }))
-
+document.querySelectorAll('.list_item').forEach((link) =>
+  link.addEventListener('click', () => {
+    //Be om hjälp för att skriva om detta
+    menuOpen = false;
+    menuButton.blur();
+    if (menuOpen) {
+      menuButton.classList.add('active');
+      menuLinks.classList.add('active');
+    } else {
+      menuButton.classList.remove('active');
+      menuLinks.classList.remove('active');
+    }
+  })
+);
 
 // Shoppingcart page toggle ----
-const toggleShoppingCartBtn = document.querySelector(".shopping_cart");
-const shoppingCartPage = document.querySelector(".shopping_basket");
-const returnToShoppingCartBtn = document.querySelector("#toggle_shopping_cart");
-toggleShoppingCartBtn.addEventListener("click", toggleOrderPage);
-returnToShoppingCartBtn.addEventListener("click", toggleOrderPage);
+const toggleShoppingCartBtn = document.querySelector('.shopping_cart');
+const shoppingCartPage = document.querySelector('.shopping_basket');
+const returnToShoppingCartBtn = document.querySelector('#toggle_shopping_cart');
+toggleShoppingCartBtn.addEventListener('click', toggleOrderPage);
+returnToShoppingCartBtn.addEventListener('click', toggleOrderPage);
 
 /****************************************************************************
  *                  Funktioner för orderpage
  ****************************************************************************/
-
 
 // For Donut order page
 
@@ -78,93 +77,54 @@ class Donut {
   }
 }
 
-
-
-
 // Donuts-array
-const donuts =
-    [new Donut(
-        "Blåbärsmunk",
-        20,
-        8,
-        "bär glasyr",
-        ["images/small/blabarsmunk_liten.jpg", "images/small/blabarsmunk2_liten.jpg"]
-    ), new Donut
-        (
-            "Chokladmunk",
-            22,
-            6,
-            "choklad",
-            ["images/small/chokladmunk_liten.jpg", "images/small/chokladmunk2_liten.jpg"]
-        ), new Donut
-        (
-            "Dubbel chokladmunk",
-            25,
-            9,
-            "choklad",
-            ["images/small/dubbelchoklad_liten.jpg", "images/small/dubbelchoklad2_liten.jpg"]
-        ), new Donut
-        (
-            "Glasyrmunk",
-            15,
-            7,
-            "glasyr",
-            ["images/small/glasymunk2_liten.jpg", "images/small/glasymunk2_liten.jpg"]
-        ), new Donut
-        (
-            "Sockermunk, glutenfri",
-            30,
-            5,
-            "socker glutenfri",
-            ["images/small/sockermunk_liten.jpg", "images/small/sockermunk2_liten.jpg",]
-        ), new Donut
-        (
-            "Strösselmunk",
-            32,
-            4,
-            "strössel glasyr",
-            ["images/small/strosselmunk_liten.jpg", "images/small/strosselmunk2_liten.jpg",]
-        ), new Donut
-        (
-            "Syltmunk, glutenfri",
-            32,
-            4,
-            "socker glutenfri",
-            ["images/small/syltmunk_liten.jpg", "images/small/syltmunk2_liten.jpg"]
-        ), new Donut
-        (
-            "Toppingmunk",
-            32,
-            6,
-            "strössel glasyr",
-            ["images/small/toppingmunk_liten.jpg", "images/small/toppingmunk2_liten.jpg"]
-        ), new Donut
-        (
-            "Vaniljmunk",
-            32,
-            6,
-            "vaniljkräm glasyr",
-            ["images/small/vaniljmunk_liten.jpg", "images/small/vaniljmunk2_liten.jpg"]
-        ), new Donut
-        (
-            "Violmunk",
-            32,
-            6,
-            "strössel glasyr",
-            ["images/small/violmunk_liten.jpg", "images/small/violmunk2_liten.jpg"]
-        )
-    ];
+const donuts = [
+  new Donut('Blåbärsmunk', 20, 8, 'bär glasyr', [
+    'images/small/blabarsmunk_liten.jpg',
+    'images/small/blabarsmunk2_liten.jpg',
+  ]),
+  new Donut('Chokladmunk', 22, 6, 'choklad', [
+    'images/small/chokladmunk_liten.jpg',
+    'images/small/chokladmunk2_liten.jpg',
+  ]),
+  new Donut('Dubbel chokladmunk', 25, 9, 'choklad', [
+    'images/small/dubbelchoklad_liten.jpg',
+    'images/small/dubbelchoklad2_liten.jpg',
+  ]),
+  new Donut('Glasyrmunk', 15, 7, 'glasyr', [
+    'images/small/glasymunk2_liten.jpg',
+    'images/small/glasymunk2_liten.jpg',
+  ]),
+  new Donut('Sockermunk, glutenfri', 30, 5, 'socker glutenfri', [
+    'images/small/sockermunk_liten.jpg',
+    'images/small/sockermunk2_liten.jpg',
+  ]),
+  new Donut('Strösselmunk', 32, 4, 'strössel glasyr', [
+    'images/small/strosselmunk_liten.jpg',
+    'images/small/strosselmunk2_liten.jpg',
+  ]),
+  new Donut('Syltmunk, glutenfri', 32, 4, 'socker glutenfri', [
+    'images/small/syltmunk_liten.jpg',
+    'images/small/syltmunk2_liten.jpg',
+  ]),
+  new Donut('Toppingmunk', 32, 6, 'strössel glasyr', [
+    'images/small/toppingmunk_liten.jpg',
+    'images/small/toppingmunk2_liten.jpg',
+  ]),
+  new Donut('Vaniljmunk', 32, 6, 'vaniljkräm glasyr', [
+    'images/small/vaniljmunk_liten.jpg',
+    'images/small/vaniljmunk2_liten.jpg',
+  ]),
+  new Donut('Violmunk', 32, 6, 'strössel glasyr', [
+    'images/small/violmunk_liten.jpg',
+    'images/small/violmunk2_liten.jpg',
+  ]),
+];
 
 const donutPlacement = document.querySelectorAll('.donut_article');
-const selectedOrderplacment = document.querySelectorAll(".selectedOrder");
-const imageHolder = document.querySelectorAll('figure');
-
-let plusbtn = document.querySelectorAll('button[data-operator="plus"]');
-let minusbtn = document.querySelectorAll('button[data-operator="minus"]');
 
 let shopCartBtnUp = '';
 let shopCartBtnDown = '';
-
 
 // Funktion lista nedan
 
@@ -193,47 +153,45 @@ function displayDonut1() {
            </div> `;
     donutPlacement[i].innerHTML = donutMarkup;
   }
-=======
 
-
-// Bild spel relaterat 
-    const nextBtns= document.querySelectorAll('.next')
-    nextBtns.forEach(btn => {
-        console.log(btn)
-        btn.addEventListener('click', nextImage);       
-    })   
-    const prevBtns = document.querySelectorAll('.prev')
-    prevBtns.forEach(btn => {  
-        btn.addEventListener('click', prevImage )
-    })
+  // Bild spel relaterat
+  const nextBtns = document.querySelectorAll('.next');
+  nextBtns.forEach((btn) => {
+    btn.addEventListener('click', nextImage);
+  });
+  const prevBtns = document.querySelectorAll('.prev');
+  prevBtns.forEach((btn) => {
+    btn.addEventListener('click', prevImage);
+  });
 }
 
 let currentImageIndex = 0;
 
 function nextImage(btn) {
-    const donutIndex= btn.currentTarget.id.replace('next-', '') //donutIndex = donutsens placering i arrayen
-    let i = 0;
-    if (currentImageIndex + 1 > donuts[i].picSrc[0].length - 1) {
-      currentImageIndex = 0;
-    } else {
-      currentImageIndex += 1;
-    }
-    document.querySelector(`#img-${donutIndex}`).setAttribute('src', donuts[donutIndex].picSrc[1] ) //plockar upp bild
-    
+  const donutIndex = btn.currentTarget.id.replace('next-', ''); //donutIndex = donutsens placering i arrayen
+  let i = 0;
+  if (currentImageIndex + 1 > donuts[i].picSrc[0].length - 1) {
+    currentImageIndex = 0;
+  } else {
+    currentImageIndex += 1;
+  }
+  document
+    .querySelector(`#img-${donutIndex}`)
+    .setAttribute('src', donuts[donutIndex].picSrc[1]); //plockar upp bild
 }
 
-function prevImage (btn){
-    const donutIndex= btn.currentTarget.id.replace('prev-', '')
-    let i = 0;
-    if (currentImageIndex - 1 < 0) {
-        currentImageIndex = donuts[i].picSrc[1].length -1;
-}  else {
-    currentImageIndex -=1;
-
+function prevImage(btn) {
+  const donutIndex = btn.currentTarget.id.replace('prev-', '');
+  let i = 0;
+  if (currentImageIndex - 1 < 0) {
+    currentImageIndex = donuts[i].picSrc[1].length - 1;
+  } else {
+    currentImageIndex -= 1;
+  }
+  document
+    .querySelector(`#img-${donutIndex}`)
+    .setAttribute('src', donuts[donutIndex].picSrc[0]);
 }
-document.querySelector(`#img-${donutIndex}`).setAttribute('src', donuts[donutIndex].picSrc[0] )
-}
-
 
 /****************************************************************************
  *                  Funktioner för shopping-basket
@@ -243,7 +201,7 @@ function displayDonutCart() {
     let donutNr = 'nr' + i;
     const cartOrderMarkup = `
             <div class="${donutNr} donuts">
-                <img src="${donuts[i].picSrc}" width="150" height="150 loading="lazy">
+                <img src="${donuts[i].picSrc[0]}" width="150" height="150 loading="lazy">
                 <h4>${donuts[i].name}</h4>
                 <ul>
                     <li>${donuts[i].price}kr</li>
@@ -277,7 +235,6 @@ function calcTotalorder() {
 function countUp(e) {
   const controll = e.currentTarget.parentElement.parentElement.attributes.class;
   const updateCounter = document.querySelectorAll('.selectcounter');
-  console.log(controll.value);
   for (let i = 0; i < donuts.length; i++) {
     if (controll.value == 'nr' + i + ' donuts') {
       donuts[i].selectCounter++;
@@ -381,7 +338,7 @@ function toggleFilterOptions() {
 }
 
 function toggleFilter(e) {
-  const filterValue = e.currentTarget.innerHTML
+  const filterValue = e.currentTarget.innerHTML;
 }
 
 // Functioner anges ovan ----------------------------
@@ -399,9 +356,10 @@ for (let i = 0; i < plusbtn.length; i++) {
 document.querySelector('#clearCartBtn').addEventListener('click', clearCart);
 const sortSelect = document.querySelector('#sorting');
 sortSelect.addEventListener('input', onSortSelect);
-document.querySelector('#mainFilterBtn').addEventListener('click', toggleFilterOptions);
+document
+  .querySelector('#mainFilterBtn')
+  .addEventListener('click', toggleFilterOptions);
 const filterButtons = document.querySelector('.filterOptions').children;
-
 
 /*
 // Filter alternativ nedan
@@ -418,8 +376,6 @@ console.log(filterButtons);
 // Filter ej klart ännu
 */
 
-
-
 //Kod för betalningsformulär
 
 const paymentForm = document.querySelector('form');
@@ -433,111 +389,109 @@ const postalAdress = document.querySelector('#postalAdress');
 const tel = document.querySelector('#tel');
 const paymentBtn = document.querySelector('#paymentBtn');
 
+paymentForm.addEventListener('submit', (e) => {
+  // e står för event
+  e.preventDefault(); //Förhindrar att skicka formuläret
 
-
-paymentForm.addEventListener('submit', (e) => { // e står för event
-    e.preventDefault(); //Förhindrar att skicka formuläret
-
-    checkInputs();
+  checkInputs();
 });
 
-function checkInputs(){
-    const firstnameValue = firstName.value.trim(); //Trim tar bort eventuellt whitespace
-    const lastnameValue = lastName.value.trim();
-    const emailValue = email.value.trim();
-    const adressValue = adress.value.trim();
-    const zipcodeValue = zipcode.value.trim();
-    const postalAdressValue = postalAdress.value.trim();
-    //const doorCode = doorCode.value.trim();
-    const telValue = tel.value.trim();
+function checkInputs() {
+  const firstnameValue = firstName.value.trim(); //Trim tar bort eventuellt whitespace
+  const lastnameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const adressValue = adress.value.trim();
+  const zipcodeValue = zipcode.value.trim();
+  const postalAdressValue = postalAdress.value.trim();
+  //const doorCode = doorCode.value.trim();
+  const telValue = tel.value.trim();
 
-    //Blir väldigt mycket upprepning av kod här, går det att förenkla med en loop trots att jag även vill kolla unika statements för varje input?
-    if(firstnameValue === ''){
-        setErrorFor(firstName, 'Du måste fylla i fältet.');
-    }else if(firstnameValue.length < 3){
-        setErrorFor(firstName, 'Ditt namn måste var längre än 3 bokstäver');
-    }else{
-        setSuccessFor(firstName);
-    }
+  //Blir väldigt mycket upprepning av kod här, går det att förenkla med en loop trots att jag även vill kolla unika statements för varje input?
+  if (firstnameValue === '') {
+    setErrorFor(firstName, 'Du måste fylla i fältet.');
+  } else if (firstnameValue.length < 3) {
+    setErrorFor(firstName, 'Ditt namn måste var längre än 3 bokstäver');
+  } else {
+    setSuccessFor(firstName);
+  }
 
-    if(lastnameValue === ''){
-        setErrorFor(lastName, 'Du måste fylla i fältet');
-    }else if(lastnameValue.length < 5){
-        setErrorFor(lastName, 'Ditt namn måste var längre än 2 bokstäver');
-    }else{
-        setSuccessFor(lastName);
-    }
+  if (lastnameValue === '') {
+    setErrorFor(lastName, 'Du måste fylla i fältet');
+  } else if (lastnameValue.length < 5) {
+    setErrorFor(lastName, 'Ditt namn måste var längre än 2 bokstäver');
+  } else {
+    setSuccessFor(lastName);
+  }
 
-    if(emailValue === ''){
-        setErrorFor(email, 'Du måste fylla i fältet');
-    }else if(!isEmail(emailValue)){
-        setErrorFor(email, 'Du måste ange en giltig E-mail');
-    }else{
-        setSuccessFor(email);
-    }
+  if (emailValue === '') {
+    setErrorFor(email, 'Du måste fylla i fältet');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Du måste ange en giltig E-mail');
+  } else {
+    setSuccessFor(email);
+  }
 
-    if(adressValue === ''){
-        setErrorFor(adress, 'Du måste fylla i fältet');
-    }else if(adressValue.length < 6){
-        setErrorFor(adress, 'Fältet måste vara längre än 6 bokstäver');
-    }else{
-        setSuccessFor(adress);
-    }
+  if (adressValue === '') {
+    setErrorFor(adress, 'Du måste fylla i fältet');
+  } else if (adressValue.length < 6) {
+    setErrorFor(adress, 'Fältet måste vara längre än 6 bokstäver');
+  } else {
+    setSuccessFor(adress);
+  }
 
-    if(zipcodeValue === ''){
-        setErrorFor(zipcode, 'Du måste fylla i fältet');
-    }else if(zipcodeValue.length < 4){
-        setErrorFor(zipcode, 'Fältet måste bestå av 4 siffror');
-    }else if(zipcodeValue.length > 4){
-        setErrorFor(zipcode, 'Du får inte ange mer än 4 siffror');
-    }else if(zipcodeValue.length == 4){
-        setSuccessFor(zipcode);
-    }
+  if (zipcodeValue === '') {
+    setErrorFor(zipcode, 'Du måste fylla i fältet');
+  } else if (zipcodeValue.length < 4) {
+    setErrorFor(zipcode, 'Fältet måste bestå av 4 siffror');
+  } else if (zipcodeValue.length > 4) {
+    setErrorFor(zipcode, 'Du får inte ange mer än 4 siffror');
+  } else if (zipcodeValue.length == 4) {
+    setSuccessFor(zipcode);
+  }
 
-    if(postalAdressValue=== ''){
-        setErrorFor(postalAdress, 'Du måste fylla i fältet');
-    }else if(postalAdressValue.length < 6){
-        setErrorFor(postalAdress, 'Fältet måste vara längre än 4 bokstäver');
-    }else{
-        setSuccessFor(postalAdress);
-    }
+  if (postalAdressValue === '') {
+    setErrorFor(postalAdress, 'Du måste fylla i fältet');
+  } else if (postalAdressValue.length < 6) {
+    setErrorFor(postalAdress, 'Fältet måste vara längre än 4 bokstäver');
+  } else {
+    setSuccessFor(postalAdress);
+  }
 
-    if(telValue === ''){
-        setErrorFor(tel, 'Du måste fylla i fältet');
-    }else if(telValue.length < 10){
-        setErrorFor(tel, 'Fältet måste bestå av 10 siffror');
-    }else if(telValue.length > 10){
-        setErrorFor(telValue, 'Du får inte ange mer än 10 siffror');
-    }else if(telValue.length == 10){
-        setSuccessFor(tel);
-    }
+  if (telValue === '') {
+    setErrorFor(tel, 'Du måste fylla i fältet');
+  } else if (telValue.length < 10) {
+    setErrorFor(tel, 'Fältet måste bestå av 10 siffror');
+  } else if (telValue.length > 10) {
+    setErrorFor(telValue, 'Du får inte ange mer än 10 siffror');
+  } else if (telValue.length == 10) {
+    setSuccessFor(tel);
+  }
 }
 
-function setErrorFor(input, message){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    const icon = formControl.querySelector('i');
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  const icon = formControl.querySelector('i');
 
-    icon.classList.remove('toggle-hidden'); //Tar bort klassen toggle-hidden vilket gör ikonen och texten synlig
-    small.classList.remove('toggle-hidden');
+  icon.classList.remove('toggle-hidden'); //Tar bort klassen toggle-hidden vilket gör ikonen och texten synlig
+  small.classList.remove('toggle-hidden');
 
-    small.innerText = message;
-    formControl.className = 'form_control error';
+  small.innerText = message;
+  formControl.className = 'form_control error';
 }
 
-function setSuccessFor(input){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    const icon = formControl.querySelector('i');
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  const icon = formControl.querySelector('i');
 
-    icon.classList.remove('toggle-hidden');
-    small.classList.add('toggle-hidden'); //Döljer texten för felmeddelande
-    formControl.className = 'form_control success';
-
+  icon.classList.remove('toggle-hidden');
+  small.classList.add('toggle-hidden'); //Döljer texten för felmeddelande
+  formControl.className = 'form_control success';
 }
 
-function isEmail(email){
-   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+function isEmail(email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
 
 /**
